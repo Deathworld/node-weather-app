5881e3ef46afb1338f0eb88dfdbd06b090d5395a
Deathworld <corentin.g@gmail.com>
2018-05-31 18:41:20 +0000

    Made the geocodeAddress function in a separate file and handled errors

---
+++ geocode/geocode.js
@@ -0,0 +1,26 @@
+const request = require('request');
+
+var geocodeAddress = (address, callback) => {
+    var encodedAddress = encodeURIComponent(address);
+    request({
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyBdUIO1NbWxwTgfpYi1ygIn7cscFQTyu3s`,
+        json: true
+    }, (error, response, body) => {
+        if(error) {
+            callback(`An error has occured => ${error}`);
+        } else if(body.status === 'ZERO_RESULTS') {
+            callback(`An error has occured => ${body.status}`);
+        } else if(body.status === 'OK') {
+            callback(undefined, {
+                address: body.results[0].formatted_address,
+                latitude: body.results[0].geometry.location.lat,
+                longitude: body.results[0].geometry.location.lng
+            });
+        } else {
+            callback(`An error has occured while getting data`);
+        }
+
+    });
+};
+
+module.exports.geocodeAddress = geocodeAddress;
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const Basics = require('./playground/async-basics');
 const request = require('request');
 const yargs = require('yargs');
 
+const geocode = require('./geocode/geocode.js');
+
 const argv = yargs.options({
     a: {
         demand: true,
@@ -17,14 +19,10 @@ const argv = yargs.options({
     .help()
     .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyBdUIO1NbWxwTgfpYi1ygIn7cscFQTyu3s`,
-    json: true
-}, (error, response, body) => {
-    console.log(`Address :  ${body.results[0].formatted_address}`);
-    console.log(`Latitude : ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
-
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if(errorMessage){
+        console.log(errorMessage);
+    } else{
+        console.log(JSON.stringify(results, undefined, 2));
+    }
 });
