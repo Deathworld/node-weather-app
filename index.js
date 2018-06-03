da2bc4ca16eb0a39c9cf60ee03d53d48b8535ac8
Deathworld <corentin.g@gmail.com>
2018-06-03 11:49:09 +0000

    Cleaned up and finished nnode-weather-app v0.1

--- index.js
+++ index.js
@@ -1,43 +1,59 @@
-const request = require('request');
 const yargs = require('yargs');
-
-const geocode = require('./geocode/geocode.js');
-const weather = require('./weather/weather');
+const axios = require('axios');
 
 const argv = yargs.options({
     a: {
-        demand: true,
+        demand: false,
         alias: 'address',
         describe: 'Address to watch for',
         string: true
+    },
+
+    d: {
+        demand: false,
+        alias: 'default',
+        describe: 'Set the default location',
+        string: true
+    },
+
+    i: {
+        demand: false,
+        alias: 'info',
+        describe: 'Print the default location',
+        string: false
     }
 })
     .help()
     .argv;
 
-// Get Latitude and Longitude from an address using Google Maps API
-// When it's done, it gets and logs the temperature using location callback
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-    if (errorMessage) {
-        console.log(errorMessage);
+
+
+
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyBdUIO1NbWxwTgfpYi1ygIn7cscFQTyu3s`;
+
+axios.get(geocodeUrl).then((response) => {
+    if(response.data.status === 'ZERO_RESULTS'){
+        throw new Error('Unable to find that address.');
+    }
+    var latitude = response.data.results[0].geometry.location.lat;
+    var longitude = response.data.results[0].geometry.location.lng;
+
+    var weatherUrl = `https://api.darksky.net/forecast/6cebf2244ad2cd5a3206fe6929dc50f1/${latitude},${longitude}`;
+
+    console.log(response.data.results[0].formatted_address);
+    return axios.get(weatherUrl);
+
+}).then((response) => {
+
+    var temperature = response.data.currently.temperature;
+    var apparentTemperature = response.data.currently.temperature;
+    console.log(`It's currently ${temperature} but it feels like ${apparentTemperature}`)
+
+}).catch((e) => {
+    if(e.code === 'ENOTFOUND'){
+        console.log('Unable to connect to API servers.');
     } else {
-        console.log(results.address);
-        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
-            if (errorMessage) {
-                console.log(errorMessage);
-            } else {
-                console.log(`It's currently ${weatherResults.temperature}F° there. It feels like ${weatherResults.apparentTemperature}`);
-            }
-        });
+        console.log(e.message);
     }
 });
-
-// Get Weather infos from Latitude and Longitude
-/*
-weather.getWeather(47.2261831,-1.5164791, (errorMessage, weatherResults) => {
-    if (errorMessage) {
-        console.log(errorMessage);
-    } else {
-        console.log(JSON.stringify(weatherResults, undefined, 2));
-    }
-});*/
